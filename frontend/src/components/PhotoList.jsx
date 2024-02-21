765eeba69f0e2e40484237d503ed7fb444c4e25b
Jonas-Kunz <jonas_K_7@outlook.com>
2024-02-21 15:54:01 -0400

    switch to mock data, add navbar

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -6,9 +6,9 @@ import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
 
-  const { sampleDataForPhotoList } = props
+  const { photos } = props
   
-  const photoListItems = sampleDataForPhotoList.map(photo => <PhotoListItem key={photo.id} urls={photo.urls} user={photo.user} location={photo.location}/>) 
+  const photoListItems = photos.map(photo => <PhotoListItem key={photo.id} urls={photo.urls} user={photo.user} location={photo.location}/>) 
   return (
     <ul className="photo-list">
       {photoListItems}
