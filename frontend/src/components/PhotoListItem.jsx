1bce9b4dc86f6fb4d8adaf1b0bff90df8b5d3bc3
Jonas-Kunz <jonas_K_7@outlook.com>
2024-02-21 12:02:26 -0400

    render multiple photos, Pass photo information to photoList.jsx from App.jsx with props, then within  PhotoList create an array of photoListItems using map to pass each item its props, render all photoListItems within photoList by placing the array within it.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,18 +2,26 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
 const PhotoListItem = (props) => {
-  const {id, imageSource, profile, username, location} = props.sampleDataForPhotoListItem;
+  const { id, location, urls, user } = props
   return (
     <article key={id} className="photo-list__item">
-      <img src={imageSource} className="photo-list__image"/>
+      <img src={urls.regular} className="photo-list__image"/>
       <footer className="photo-list__user-details">
-        <img src={profile} className="photo-list__user-profile"/>
+        <img src={user.profile} className="photo-list__user-profile"/>
         <div className="photo-list__user-info" >
-          {username}
+          {user.username}
           <div className="photo-list__user-location">{location.city}, {location.country}</div> 
         </div>
       </footer>
