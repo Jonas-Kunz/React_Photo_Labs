27ef6d0af7a6b9fa99d4a774fa218774b6e26b94
Jonas-Kunz <jonas_K_7@outlook.com>
2024-02-21 10:18:11 -0400

    remove unessecary template literals in photolistitme.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -8,12 +8,12 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   const {id, imageSource, profile, username, location} = props.sampleDataForPhotoListItem;
   return (
-    <article key={`${id}`}>
-      <img src={`${imageSource}`}/>
+    <article key={id}>
+      <img src={imageSource}/>
       <footer>
-        <img src={`${profile}`}/>
-        <p>{`${username}`}</p>
-        <p>{`${location.city}, ${location.country}`}</p>
+        <img src={profile}/>
+        <p>{username}</p>
+        <p>{location.city}, {location.country}</p>
       </footer>
     </article>
   )
